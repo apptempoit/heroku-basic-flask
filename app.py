f5a3346cb82c24ff25430f5d1c629150400bdc5d
apptempoit <34367079+apptempoit@users.noreply.github.com>
2019-07-10 15:30:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def createcm(summary=None, change=None):
 def hello_world():  #def  เป็นคำสำคัญสำหรับการสร้างฟังก์ชัน
    return 'Hello World' #ให้แสดงข้อความว่า Hello World ออกทางหน้าฟอร์ม
 
+@app.route('/button')
 def button():  
    return '<meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"><a href"https://pythonc3.herokuapp.com/createcm/25/25" class="w3-btn w3-blue">ส่งค่า</a>'
 
