2db513863118ea1993776e56e2452e2c8eef57d3
apptempoit <34367079+apptempoit@users.noreply.github.com>
2019-08-05 14:06:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,47 @@ import pickle as p
 import json
 from numpy.core import multiarray
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
 
-@app.route('/api/', methods=['POST'])
-def makecalc():
-    data = request.get_json()
-    modelfile = 'final_prediction.pickle'
-    model = p.load(open(modelfile, 'rb'))
-    prediction = np.array2string(model.predict(data))
+#@app.route('/api/', methods=['POST'])
+#def makecalc():
+#    data = request.get_json()
+#    modelfile = 'final_prediction.pickle'
+#    model = p.load(open(modelfile, 'rb'))
+#    prediction = np.array2string(model.predict(data))
 
-    return jsonify(prediction)
+#    return jsonify(prediction)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
+
+    
+    
+    
+app=Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return flask.render_template('index.html')
+
+
+def ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1,12)
+    loaded_model = pickle.load(open("model.pkl","rb"))
+    result = loaded_model.predict(to_predict)
+    return result[0]
+
+@app.route('/result',methods = ['POST'])
+def result():
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()
+        to_predict_list=list(to_predict_list.values())
+        to_predict_list = list(map(int, to_predict_list))
+        result = ValuePredictor(to_predict_list)
+        if int(result)==1:
+            prediction='Income more than 50K'
+        else:
+            prediction='Income less that 50K'
+        return render_template("result.html",prediction=prediction)
