02838c770e949af5957d1612a54de9dbbbd08c65
apptempoit <34367079+apptempoit@users.noreply.github.com>
2019-08-08 00:37:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def index():
 
 
 def ValuePredictor(to_predict_list):
-    to_predict = np.array(to_predict_list).reshape(1,12)
+    to_predict = np.array(to_predict_list).reshape(1,10)
     loaded_model = pickle.load(open("DT_model.pkl","rb"))
     result = loaded_model.predict(to_predict)
     return result[0]
